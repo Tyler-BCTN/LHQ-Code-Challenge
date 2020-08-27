4e54db7402fb20f0047b76ea7166548fe78ef7cb
Tyler <tylerdanielphotography>
2020-08-27 16:28:51 -0500

    blank repo with on load api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      teams: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://statsapi.web.nhl.com/api/v1/standings")
+      .then(response => response.json())
+      .then(data => this.setState({ teams: data.records }));
+  }
+
+  render() {
+    console.log(this.state.teams)
+    return (
+      <div className="App">
+        <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
+          Data...
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
